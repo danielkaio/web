c5692e2f99d49a7864a2ab06e4dbddd91b4215ad
daniel <dani.ajala@yahoo.com>
2021-09-03 00:12:04 -0300

    sass

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'))
+const browserSync = require('browser-sync').create()
+
+
+const caminho = 'scss/**/*.scss'
+
+const estilo = () => {
+    return gulp.src(caminho)
+        .pipe(sass())
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream())
+}
+
+const watch = () => {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+
+    })
+
+
+    gulp.watch(caminho, estilo).on('change', browserSync.reload)
+}
+
+
+exports.estilo = estilo
+exports.watch = watch
